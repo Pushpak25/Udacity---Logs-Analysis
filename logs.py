5873e1ea52f0562fd260485a4e6556e0775ecf82
Pushpak <mpushpakteja@gmail.com>
2017-07-07 20:31:09 +0530

    Add files via upload

---
+++ logs.py
@@ -0,0 +1,81 @@
+import psycopg2
+
+
+def connect(database_name="news"):
+    """Connect to given PostgreSQL database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect("dbname={dbname}".format(dbname=database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except Exception, error:
+        print error
+
+
+def execute_query(query):
+    """Executes a query and return it's results"""
+    db, cursor = connect()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    db.close()
+    return results
+
+
+def print_output(results, ext):
+    """Writes results from execute_query in a file"""
+    f = open("output.txt", "a")
+    for line in results:
+        f.write("{}     - {}{}\n".format(str(line[0]), str(line[1]), str(ext)))
+    f.close()
+
+# Most popular three articles of all time:
+query_1 = (
+    "select articles.title, count(*) as num_views from articles \
+    inner join log on log.path like concat('%',articles.slug,'%') \
+    group by articles.title, log.path order by num_views desc limit 3"
+)
+
+# Most popular authors of all time
+query_2 = (
+    "select authors.name, count(*) as num_views from articles \
+    inner join authors on authors.id= articles.author \
+    inner join log on log.path like concat('%',articles.slug,'%') \
+    group by authors.name, log.path order by num_views desc limit 4"
+)
+
+# Days with more than 1% failed requests
+
+query_3 = (
+    "select day,per_errors from ( \
+    select day, round((sum(requests)/(select count(1) from log \
+    where date(log.time) = day) * 100), 2) as per_errors from ( \
+    select date(log.time) as day, count(1) as requests \
+    from log where status like '%404%' \
+    group by day) as per_logs \
+    group by day order by per_errors desc) \
+    as query_3 where per_errors > 1;"
+)
+
+if __name__ == '__main__':
+    # Problem 1
+    f = open("output.txt", "w")
+    f.write("What are the most popular three articles of all time?\n")
+    f.close()
+
+    results = execute_query(query_1)
+    print_output(results, " views")
+
+    # Problem 2
+    f = open("output.txt", "a")
+    f.write("\n\nWho are the most popular article authors of all time?\n")
+    f.close()
+
+    results = execute_query(query_2)
+    print_output(results, " views")
+
+    # Problem 3
+    f = open("output.txt", "a")
+    f.write("\n\nOn which days did more than 1% of requests lead to errors?\n")
+    f.close()
+
+    results = execute_query(query_3)
+    print_output(results, "% errors")
